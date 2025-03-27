44f00c44f1aff2537c3a1cbf0e67dfdac7f34d67
ismail.e <ismail.e@opensenselabs.com>
2025-03-27 12:13:14 +0530

    Updates

---
+++ precache-manifest.9b48acdda288d4b32741040faa2d5dfe.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "01b9f1b60ec055e4dfd2f2d2d1f95087",
+    "url": "/facelook/index.html"
+  },
+  {
+    "revision": "e364110c7817526c495c",
+    "url": "/facelook/static/css/2.31dafeb2.chunk.css"
+  },
+  {
+    "revision": "e686492c858312b87db3",
+    "url": "/facelook/static/css/main.4ff7780b.chunk.css"
+  },
+  {
+    "revision": "e364110c7817526c495c",
+    "url": "/facelook/static/js/2.9159a55b.chunk.js"
+  },
+  {
+    "revision": "e686492c858312b87db3",
+    "url": "/facelook/static/js/main.56febd2d.chunk.js"
+  },
+  {
+    "revision": "e51726415297c9a85f61",
+    "url": "/facelook/static/js/runtime~main.404ab61c.js"
+  }
+]);
--- src/components/Rank/Rank.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-const Rank =()=>{
-	return(
-<div className="black f2 white">Image Recognition</div>
-		);
-}
-
-export default Rank;
